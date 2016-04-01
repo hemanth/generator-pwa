b5ee7ecef5e27545425815d207170ae1eecc2c05
Hemanth.HM <hemanth.hm@gmail.com>
2016-04-01 16:28:26 +0530

    Pre-test makes no sense in this case.

--- gulpfile.js
+++ gulpfile.js
@@ -9,43 +9,34 @@ var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    return gulp.src('**/*.js')
+      .pipe(excludeGitignore())
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
 });
 
 gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
+    nsp({ package: path.resolve('package.json') }, cb);
 });
 
-gulp.task('pre-test', function () {
-  return gulp.src('generators/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(istanbul({
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire());
-});
+gulp.task('test', function (cb) {
+    var mochaErr;
 
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
-
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
+    gulp.src('test/**/*.js')
+      .pipe(plumber())
+      .pipe(mocha({ reporter: 'spec' }))
     .on('error', function (err) {
-      mochaErr = err;
+        mochaErr = err;
     })
     .pipe(istanbul.writeReports())
     .on('end', function () {
-      cb(mochaErr);
+        cb(mochaErr);
     });
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
+    gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
 });
 
 gulp.task('prepublish', ['nsp']);
