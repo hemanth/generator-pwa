6d852e13b230299e9251494777603bd8c2820e3e
gokulkrishh <krishnangokul9@gmail.com>
2016-05-09 10:30:54 +0530

    Fix for test cases

--- test/app.js
+++ test/app.js
@@ -1,17 +1,27 @@
 'use strict';
+
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-pwa:app', function () {
-    before(function (done) {
-        helpers.run(path.join(__dirname, '../generators/app'))
-          .withOptions({ apiKey: 123 })
-          .withPrompts({ isPush: true, gcmSenderId:false })
-          .on('end', done);
-    });
+var prompts = {
+  appName: 'test',
+  isPush: true,
+  apiKey: 123,
+  gcmSenderId: 123
+};
 
-    it('creates files', function () {
-        assert.file('css favicon images index.html js manifest.json sw-cache-polyfill.js sw.js'.split(' '));
-    });
+var options = {};
+
+describe('generator-pwa:app', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+    .withOptions(options)
+    .withPrompts(prompts)
+    .on('end', done);
+  });
+
+  it('creates files', function () {
+    assert.file('test/css test/css/styles.css test/favicon.ico test/images test/index.html test/js test/js/app.js test/manifest.json test/sw.js test/server.js'.split(' '));
+  });
 });
